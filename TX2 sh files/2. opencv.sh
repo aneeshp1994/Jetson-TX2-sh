2b7a791ba2746549a380ad3fdb9b4fe4367d6794
aneeshp1994 <32482880+aneeshp1994@users.noreply.github.com>
2018-04-09 10:44:03 +0530

    Add files via upload

---
+++ TX2 sh files/2. opencv.sh
@@ -0,0 +1,83 @@
+# Install the necessities using pip
+pip install --upgrade pip
+
+pip install --upgrade numpy
+
+pip install --upgrade scipy
+
+sudo apt-get install --upgrade -y \
+    libglew-dev \
+    libtiff5-dev \
+    zlib1g-dev \
+    libjpeg-dev \
+    libpng12-dev \
+    libjasper-dev \
+    libavcodec-dev \
+    libavformat-dev \
+    libavutil-dev \
+    libpostproc-dev \
+    libswscale-dev \
+    libeigen3-dev \
+    libtbb-dev \
+    libgtk2.0-dev \
+    cmake \
+    pkg-config
+
+cd $HOME
+
+mkdir work
+
+cd work
+
+# Clone the git repository of opencv and opencv-contrib
+git clone https://github.com/opencv/opencv.git
+git clone https://github.com/opencv/opencv_contrib.git
+
+sudo apt-get install --upgrade -y python3-dev python3-py python3-pytest
+
+sudo apt-get install --upgrade -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
+
+cd opencv
+
+mkdir build
+
+cd build
+
+cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=/usr \
+    -DBUILD_PNG=OFF \
+    -DBUILD_TIFF=OFF \
+    -DBUILD_TBB=OFF \
+    -DBUILD_JPEG=OFF \
+    -DBUILD_JASPER=OFF \
+    -DBUILD_ZLIB=OFF \
+    -DBUILD_EXAMPLES=OFF \
+    -DBUILD_opencv_java=OFF \
+    -DBUILD_opencv_python2=OFF \
+    -DBUILD_opencv_python3=ON \
+    -DENABLE_PRECOMPILED_HEADERS=OFF \
+    -DWITH_OPENCL=OFF \
+    -DWITH_OPENMP=OFF \
+    -DWITH_FFMPEG=ON \
+    -DWITH_GSTREAMER=ON \
+    -DWITH_GSTREAMER_0_10=OFF \
+    -DWITH_CUDA=ON \
+    -DWITH_GTK=ON \
+    -DWITH_VTK=OFF \
+    -DWITH_TBB=ON \
+    -DWITH_1394=OFF \
+    -DWITH_OPENEXR=OFF \
+    -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-8.0 \
+    -DCUDA_ARCH_BIN=6.2 \
+    -DCUDA_ARCH_PTX="" \
+    -DINSTALL_C_EXAMPLES=OFF \
+    -DINSTALL_TESTS=OFF \
+    -DOPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
+../
+
+# Make and install opencv
+make -j6
+
+sudo make install
+
