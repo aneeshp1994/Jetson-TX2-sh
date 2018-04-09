75d8e55445d6c3726f71250653d15dce55efa678
aneeshp1994 <32482880+aneeshp1994@users.noreply.github.com>
2018-04-09 16:03:31 +0530

    Add files via upload

---
+++ 1. prerequisites.sh
@@ -0,0 +1,18 @@
+cd $HOME
+
+# Enable all the cores of CPU
+sudo nvpmodel -m 0
+
+sudo apt-get update && sudo apt-get upgrade
+
+# Add aliases to pip3.5 and python3.5
+echo alias pip=\”sudo -H pip3\” >> .bashrc
+echo alias python=\”python3\” >> .bashrc
+export LC_ALL=C
+
+# Install some necessities before restarting
+sudo apt-get install --upgrade git
+sudo apt-get install python3-pip
+
+# Tell user that the system will restart to let the changes take effect
+echo 'Restart the system for the changes to take effect.'
