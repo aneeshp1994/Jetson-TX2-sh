9419d4f3eb037e05ec05bb48aa5e350b0a3c1c6b
aneeshp1994 <aneeshp1994@gmail.com>
2018-04-11 14:29:57 +0530

    Added commands to install php version 5.6

---
+++ 4. install MySQL Apache2Php.sh
@@ -0,0 +1,12 @@
+# Install MySQL
+sudo apt-get update
+sudo apt-get install --upgrade -y mysql-server
+sudo apt-get install --upgrade python3-mysqldb
+
+# Install apache2
+sudo apt-get install --upgrade -y apache2
+
+# Install php
+sudo add-apt-repository ppa:ondrej/php
+sudo apt-get update
+sudo apt-get install --upgrade php5.6
